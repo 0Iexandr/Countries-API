00f363be8fb898a632dab95a9b529623200b9a6f
Pavel <pavel.filkovskiy@gmail.com>
2022-09-03 22:26:33 +0100

    refactor

--- script.js
+++ script.js
@@ -75,66 +75,66 @@ function selectSubregion(event) {
     countriesContainer.replaceChildren([]);
     countriesContainer.style.visibility = '';
     countriesFactsContainer.style.display = 'none';
-    
-    const countries = regions.filter(region => region.subregion === event.target.value);
-    
-    countries.forEach(country => {
-        const countryName = document.createElement('h');
-        countryName.className = 'country-name';
-        countryName.innerText = country.name.official;
+        
+    countries
+        .filter(country => country.subregion === event.target.value)
+        .forEach(country => {
+            const countryName = document.createElement('h');
+            countryName.className = 'country-name';
+            countryName.innerText = country.name.official;
 
-        const countryFlag = document.createElement('img');
-        countryFlag.src = country.flags.png;
+            const countryFlag = document.createElement('img');
+            countryFlag.src = country.flags.png;
 
-        countriesContainer.appendChild(countryName);
-        countriesContainer.appendChild(countryFlag);
+            countriesContainer.appendChild(countryName);
+            countriesContainer.appendChild(countryFlag);
 
-        countryName.addEventListener('click', () => {
-            countriesFactsContainer.replaceChildren([]);
-            countriesFactsContainer.style.display = 'block';
+            countryName.addEventListener('click', () => {
+                countriesFactsContainer.replaceChildren([]);
+                countriesFactsContainer.style.display = 'block';
 
-            const countryFacts = document.createElement('h');
-            countryFacts.innerText = 'Facts:';
-            countryFacts.className = 'facts-title';
-            countriesFactsContainer.appendChild(countryFacts);
+                const countryFacts = document.createElement('h');
+                countryFacts.innerText = 'Facts:';
+                countryFacts.className = 'facts-title';
+                countriesFactsContainer.appendChild(countryFacts);
 
-            const countryCapital = document.createElement('h3');
-            countryCapital.innerText = `capital: ${country.capital}`;
-            countriesFactsContainer.appendChild(countryCapital);
+                const countryCapital = document.createElement('h3');
+                countryCapital.innerText = `capital: ${country.capital}`;
+                countriesFactsContainer.appendChild(countryCapital);
 
-            const countryPopulation = document.createElement('h3');
-            countryPopulation.innerText = `population: ${country.population}`;
-            countriesFactsContainer.appendChild(countryPopulation);
-            
-            const countryArea = document.createElement('h3');
-            countryArea.innerText = `area: ${country.area}`;
-            countriesFactsContainer.appendChild(countryArea);
+                const countryPopulation = document.createElement('h3');
+                countryPopulation.innerText = `population: ${country.population}`;
+                countriesFactsContainer.appendChild(countryPopulation);
+                
+                const countryArea = document.createElement('h3');
+                countryArea.innerText = `area: ${country.area}`;
+                countriesFactsContainer.appendChild(countryArea);
 
-            const countryCurrencies = document.createElement('ul');
-            countryCurrencies.innerText = 'currencies:';
-            for(currency in country.currencies) {
-                const countryCurrency = document.createElement('li');
-                countryCurrency.innerText = `${currency} : ${country.currencies[currency].name} : ${country.currencies[currency].symbol}`;
-                countryCurrencies.appendChild(countryCurrency);
-            };
-            countriesFactsContainer.appendChild(countryCurrencies);
+                const countryCurrencies = document.createElement('ul');
+                countryCurrencies.innerText = 'currencies:';
+                for(currency in country.currencies) {
+                    const countryCurrency = document.createElement('li');
+                    countryCurrency.innerText = `${currency} : ${country.currencies[currency].name} : ${country.currencies[currency].symbol}`;
+                    countryCurrencies.appendChild(countryCurrency);
+                };
+                countriesFactsContainer.appendChild(countryCurrencies);
 
-            const countryLanguages = document.createElement('ul');
-            countryLanguages.innerText = 'languages:';
-            for(language in country.languages) {
-                const countryLanguage = document.createElement('li');
-                countryLanguage.innerText = `${language} : ${country.languages[language]}`;
-                countryLanguages.appendChild(countryLanguage);
-            };
-            countriesFactsContainer.appendChild(countryLanguages);
+                const countryLanguages = document.createElement('ul');
+                countryLanguages.innerText = 'languages:';
+                for(language in country.languages) {
+                    const countryLanguage = document.createElement('li');
+                    countryLanguage.innerText = `${language} : ${country.languages[language]}`;
+                    countryLanguages.appendChild(countryLanguage);
+                };
+                countriesFactsContainer.appendChild(countryLanguages);
 
-            const countryViewOnMap = document.createElement('a');
-            countryViewOnMap.innerText = 'view on map';
-            countryViewOnMap.href = country.maps.openStreetMaps;
-            countryViewOnMap.target = "_blank";
-            countriesFactsContainer.appendChild(countryViewOnMap);
+                const countryViewOnMap = document.createElement('a');
+                countryViewOnMap.innerText = 'view on map';
+                countryViewOnMap.href = country.maps.openStreetMaps;
+                countryViewOnMap.target = "_blank";
+                countriesFactsContainer.appendChild(countryViewOnMap);
+            });
         });
-    });
 };
 
 window.onload = setup;
